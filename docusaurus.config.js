169d004c5bca90334ac1291863bc97f4e4086034
Clement Toni <clement.toni@epitech.eu>
2025-01-07 15:11:27 +0000

    update landing page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,10 +15,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://clementtoni.github.io',
+  url: 'https://clementtoni.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/test-bulman/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,32 +4,29 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Efficient server solution',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Server meshing, is today the most efficient solution to handle a large number of players in a single game world.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Implementable in your project',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Celte is a server meshing solution under the form of an <code>SDK</code>, Implementable in any custom game engine.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Scalable',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Celte is designed to be scalable and can adapt to the growth of your project.
       </>
     ),
   },
